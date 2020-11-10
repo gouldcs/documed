3502f833795c2747d0aaac168531cc8c69fddd65
Amanda Marques <amarqu21@lion.lmu.edu>
2020-11-09 19:52:31 -0800

    Made storybook work

--- milestone3/src/pages/dashboard/Dashboard.jsx
+++ milestone3/src/pages/dashboard/Dashboard.jsx
@@ -1,62 +1,62 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
-import Paper from '@material-ui/core/Paper'
+import Paper from "@material-ui/core/Paper";
 
-const useStyles = makeStyles(theme => ({
-
-    paperContainer: {
-    display: 'flex',
+const useStyles = makeStyles((theme) => ({
+  paperContainer: {
+    display: "flex",
     flex: 1,
-    flexDirection: 'column',
+    flexDirection: "column",
 
-    backgroundColor: 'lightblue',
+    backgroundColor: "lightblue",
     padding: theme.spacing(5),
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
+}));
 
-}))
-
-const Dashboard = props => {
-  const classes = useStyles(props)
+const Dashboard = (props) => {
+  const classes = useStyles(props);
   const [values, setValues] = React.useState({
-    amount: '',
-    password: '',
-    weight: '',
-    weightRange: '',
+    amount: "",
+    password: "",
+    weight: "",
+    weightRange: "",
     showPassword: false,
-  })
+  });
 
-  const handleChange = prop => event => {
-    setValues({ ...values, [prop]: event.target.value })
-  }
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
 
   const handleClickShowPassword = () => {
-    setValues({ ...values, showPassword: !values.showPassword })
-  }
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
 
-  const handleMouseDownPassword = event => {
-    event.preventDefault()
-  }
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
-  const [anchorEl, setAnchorEl] = React.useState(null)
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-  const handleClick = event => {
-    setAnchorEl(event.currentTarget)
-  }
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
   const handleClose = () => {
-    setAnchorEl(null)
-  }
+    setAnchorEl(null);
+  };
 
   return (
     <>
-     <div className={classes.paperContainer}>
-        <Paper className={classes.paper} variant="outlined"></Paper>
-    </div>
+      <div className={classes.paperContainer}>
+        <Paper className={classes.paper} variant="outlined">
+          hi
+        </Paper>
+      </div>
     </>
-      )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- milestone3/src/pages/dashboard/dashboard.stories.jsx
+++ milestone3/src/pages/dashboard/dashboard.stories.jsx
@@ -1,11 +1,8 @@
-import React from 'react'
-import centered from '@storybook/addon-centered/react'
-import { withKnobs } from '@storybook/addon-knobs'
-import Dashboard from './Dashboard'
+import React from "react";
+import Dashboard from "./Dashboard";
 
 export default {
-  title: 'pages/dashboard/Dashboard',
-  decorators: [centered, withKnobs],
-}
+  title: "pages/dashboard/Dashboard",
+};
 
-export const basic = () => <Dashboard />
+export const basic = () => <Dashboard />;
