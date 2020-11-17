bfa7c8c792a035ff965f2fc0189a44d6e8d7631c
Cameron Gould <cgould18@georgefox.edu>
2020-11-16 23:17:09 -0800

    added drag component

--- milestone3/src/components/DragToUpload.jsx
+++ milestone3/src/components/DragToUpload.jsx
@@ -8,16 +8,19 @@ const useStyles = makeStyles(
     (theme) => ({
 
       outlineContainer: {
-        width: 250,
-        height: 250,
+        width: 450,
+        height: 438,
         backgroundImage: 'url("data:image/svg+xml,%3csvg width=\'100%25\' height=\'100%25\' xmlns=\'http://www.w3.org/2000/svg\'%3e%3crect width=\'100%25\' height=\'100%25\' fill=\'none\' rx=\'45\' ry=\'45\' stroke=\'grey\' stroke-width=\'15\' stroke-dasharray=\'25\' stroke-dashoffset=\'0\' stroke-linecap=\'round\'/%3e%3c/svg%3e")',
         borderRadius: 45,
+        justifyContent: "center",
+        alignContent: "center",
+
       },
 
       dragPrompt: {
         color: "grey",
-        justifyContent: "center",
-        alignItems: "center",
+        align: "center",
+        paddingTop: "45%",
       },
     
 }))
@@ -27,8 +30,8 @@ const DragToUpload = (props) => {
 
   return (
     <div className={classes.outlineContainer}>
-      <Typography className={classes.dragPrompt}>
-        Drag to upload
+      <Typography className={classes.dragPrompt} variant="h4" align="center">
+        Drag documents here
       </Typography>
     </div>
   )
