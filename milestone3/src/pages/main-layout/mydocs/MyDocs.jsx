bd52228aa33afc7273e6c70408b3e63c7203e073
MichaelReza <45471734+MichaelReza@users.noreply.github.com>
2020-11-26 02:27:51 -0800

    Update MyDocs.jsx

--- milestone3/src/pages/main-layout/mydocs/MyDocs.jsx
+++ milestone3/src/pages/main-layout/mydocs/MyDocs.jsx
@@ -54,7 +54,7 @@ const useStyles = makeStyles((theme) => ({
       textDecoration: "none",
     },
 
-    /// Cancel/Done buttons
+    /// button container
     optionsContainer: {
       display: "flex",
       flexDirection: "row",
